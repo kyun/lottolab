e2676482ff922840f73a6e6bd3eabb687affd40a
zynkn <wls5563@gmail.com>
2019-12-08 18:53:38 +0900

    세팅ㄷ중

---
+++ lambda/lotto.ts
@@ -0,0 +1,68 @@
+import AWS from "aws-sdk";
+import axios from "axios";
+import moment from "moment-timezone";
+const DYNAMO_DB = new AWS.DynamoDB.DocumentClient();
+const LOTTO_URL = "http://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo=";
+
+
+export const updateBot = async (event:any) =>{
+  try{
+    let now = moment().tz("Asia/Seoul");
+    let start = moment("2002-12-02");
+    let no = ~~(moment.duration(now.diff(start)).asWeeks())+1;
+    const {data} = await axios.get(`${LOTTO_URL}${no}`);
+    if(data.returnValue !== "success" || data.totSellamnt === 0) {
+      throw {message: `${now.format('YYYY-MM-DD HH:mm:ss')}::Fail to get ${no}th winning numbers`}
+    }
+    const price = no < 88 ? 2000 : 1000;
+    const params = {
+      TableName: "lottolab",
+      Item: {
+        id: 1+"",
+        price: price,
+        year: moment(data.drwNoDate).format("YYYY"),
+        month: moment(data.drwNoDate).format("MM"),
+        total: data.totSellamnt / price,
+        createdAt: moment().tz("Asia/Seoul").format("YYYY-MM-DD HH:mm:ss"),
+        ...data
+      },
+      Expected: {
+        "id": {
+          "Exists": false
+        }
+      }
+    }
+    const res:any = await DYNAMO_DB.put(params, (err,res)=>{
+      if(err) {
+        console.log("_!@+@!@#!@#+!@#+!@");
+        console.log(JSON.stringify(err));
+        console.log('db error');
+        return {
+          statusCode: 500,
+          body: JSON.stringify({...err, message: 'while put data to db'})
+        }
+      }
+      console.log(res);
+      return res;
+    }).promise();
+    console.log(res);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'success',
+        res: {...res}
+      })
+    }
+  }catch(e){
+    console.log("_!@+@!@#!@#+!@#+!@");
+    console.log(JSON.stringify(e));
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        ...e,
+        msg: 'errror'
+      })
+    }
+  }
+
+};
