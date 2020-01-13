b9aa4bd0c5a2001dde60f26a742a0f39e1bbe80f
zynkn <wls5563@gmail.com>
2020-01-13 21:02:27 +0900

    chart!

--- client/components/List/LottoItem.tsx
+++ client/components/List/LottoItem.tsx
@@ -1,34 +1,87 @@
 import React from 'react';
+import * as Recharts from 'recharts';
 
+const {LineChart, Line, ResponsiveContainer} = Recharts;
+const data = [
+      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+];
 const LottoItem:React.FC<any> = () => {
   
   return (
     <div className="LottoItem">
       <div className="head">
-        <span>893회차 당첨번호</span>
+        <span className="name"><b>893</b> 회차 당첨번호</span>
         <span>2020년 01월 12일</span>
       </div>
       <div className="content">
-        <div className={`LottoBall`}>
-          <span>1</span>
+        <div>
+          <div className={`LottoBall`}>
+            <span>1</span>
+          </div>
+          <div className={`LottoBall`}>
+            <span>1</span>
+          </div>
+          <div className={`LottoBall`}>
+            <span>1</span>
+          </div>
+          <div className={`LottoBall`}>
+            <span>1</span>
+          </div>
+          <div className={`LottoBall`}>
+            <span>1</span>
+          </div>
+          <div className={`LottoBall`}>
+            <span>1</span>
+          </div>
+          <div className={`LottoBall --bonus`}>
+            <span>1</span>
+          </div>
         </div>
-        <div className={`LottoBall`}>
-          <span>1</span>
+      </div>
+      <div className="foot">
+        
+
+        <div className="box">
+          <div className="chart-wrap">
+            <ResponsiveContainer width="100%" height="100%">
+              <LineChart data={data}>
+                <Line type='monotone' dot={false} dataKey='pv' stroke='rgba(255, 78, 120,0.7)' strokeWidth={3} />
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
+          <span className="value">234억 1234만원</span>
+          <span className="label">1등 당첨금</span>
         </div>
-        <div className={`LottoBall`}>
-          <span>1</span>
+        <div className="box">
+        <div className="chart-wrap">
+            <ResponsiveContainer width="100%" height="100%">
+              <LineChart data={data}>
+                <Line type='monotone' dot={false} dataKey='pv' stroke='rgba(255, 78, 120,0.7)' strokeWidth={3} />
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
+          <span className="value">1234만 1234게임</span>
+          <span className="label">판매량</span>
         </div>
-        <div className={`LottoBall`}>
-          <span>1</span>
-        </div>
-        <div className={`LottoBall`}>
-          <span>1</span>
+        <div className="box">
+        <span className="value">7명</span>
+        <span className="label">1등 당첨자</span>
         </div>
       </div>
       <style jsx>{`
         .LottoItem{
-          background: rgba(255,255,255,0.1);
-          height: 200px;
+          //background: rgba(255,255,255,0.1);
+          //height: 200px;
+          border-bottom: 2px solid #49668A;
+        }
+        .LottoItem + .LottoItem{
+          margin-top: 8px;
         }
         .head{
           color: white;
@@ -36,19 +89,72 @@ const LottoItem:React.FC<any> = () => {
           padding: 8px 16px;
           display: flex;
           justify-content: space-between;
+          align-items: center;
+          margin-bottom: 8px;
+          padding-top: 12px;
+        }
+        .head > .name{
+          font-size: 14pt;
+        }
+        .content{
+          text-align: center;
+          margin-bottom: 16px;
         }
 
         .LottoBall{
           display: inline-flex;
           align-items: center;
           justify-content: center;
-          width: 28px;
-          height: 28px;
+          width: 36px;
+          height: 36px;
           background: red;
           border-radius:3px;
+          font-size: 14pt;
+          font-weight: bold;
+          color: white;
         }
+        
         .LottoBall + .LottoBall{
           margin-left: 4px;
+        }
+        .LottoBall + .LottoBall.--bonus{
+          margin-left: 16px;
+        }
+
+        .foot{
+          position: relative;
+          display: flex;
+
+        }
+        .foot > .box > .chart-wrap{
+          z-index: 0;
+          width: calc(100%);
+          height: 100%;
+          position: absolute;
+          left: 0;
+          top:0;
+        }
+        .foot > .box{
+          background: rgba(255,255,255,0.05);
+          position: relative;
+          z-index: 1;
+          display: inline-flex;
+          flex-direction: column;
+          flex: 1 0 0;
+          text-align: center;
+          padding-bottom: 16px;
+          padding-top: 16px;
+        }
+        .foot > .box > .value{
+          z-index: 1;
+          color: #83E4F1;
+          margin-bottom: 4px;
+          font-weight: bold;
+        }
+        .foot > .box > .label{
+          color: #fff;
+          font-size: 0.9em;
+          z-index: 1;
         }
       `}</style>
     </div>
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -4,12 +4,136 @@ import Layout from '../components/Layout'
 import { NextPage } from 'next'
 import Tabbar from '../components/common/Tabbar'
 import LottoItem from '../components/List/LottoItem'
+import * as Recharts from 'recharts';
 
+const randoms = () => {
+  return  (Math.floor(Math.random() * 100) + 50)
+}
+const {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer} = Recharts;
+const data = [
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+
+  {name: '1', uv: randoms(), pv: randoms(), amt: randoms()},
+
+];
 const IndexPage: NextPage = () => {
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       {/* <h1>123</h1> */}
-      <LottoItem />
+      {/* <LottoItem />
+      <LottoItem /> */}
+      <div className="chart-wrap">
+        <div className="xAxis">
+          <span>1</span>
+          <span>2</span>
+          <span>3</span>
+          <span>4</span>
+          <span>5</span>
+          <span>6</span>
+          <span>7</span>
+          <span>8</span>
+          <span>9</span>
+          <span>10</span>
+          <span>11</span>
+          <span>12</span>
+
+        </div>
+        <ResponsiveContainer width={'100%'} height={200}>
+          
+          <LineChart data={data} margin={{ top: 0, right: 0, bottom: 0, left: 0 }}>
+
+            <CartesianGrid strokeDasharray="0" horizontal={false}/>
+            <Tooltip/>
+            <XAxis interval={3}  hide={true}/>
+            <Line dataKey="pv" dot={false} stroke="#8884d8" strokeWidth={4} activeDot={{r: 8}}/>
+            <Line dataKey="uv" dot={false} stroke="#82ca9d" strokeWidth={4} />
+            <Line dataKey="amt" dot={false} stroke="#ff2489" strokeWidth={4} />
+          </LineChart>
+
+        </ResponsiveContainer>
+       
+      </div>
+      <style jsx>{`
+        .chart-wrap{
+          position: relative;
+        }
+        .chart-wrap > .xAxis{
+          position: absolute;
+          width: 100%;
+          height: 100%;
+
+        }
+        .chart-wrap > .xAxis > span{
+          box-sizing: border-box;
+          display: inline-block;
+          padding-top: 8px;
+          border-top: 2px solid rgba(255,255,255,0.4);
+          text-align: center;
+          color: rgba(255,255,255,0.7);
+          width: calc(100%/ 12);
+          height: 100%;
+          background-color: rgba(255,255,255,0.1);
+        }
+      `}</style>
+
     </Layout>
   )
 }
